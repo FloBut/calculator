e3d0cfad19a573c9108db569e8692ceca51725b4
flori.butuc <f.butuc5@gmail.com>
2023-08-08 10:26:31 +0300

    calculator

---
+++ Main.java
@@ -0,0 +1,14 @@
+package EX_OOP.ex_2_calculator;
+
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
+import static EX_OOP.ex_2_calculator.Calculate.diffNo;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Calculate.sumNo());
+        System.out.println(Calculate.diffNo());
+        System.out.println(Calculate.factorialNo());
+    }
+}
